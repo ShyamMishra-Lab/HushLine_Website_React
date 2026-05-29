ced0f52bbaa9d8f2d41c2feecca502e4e8103a30
Shyam Mishra <108shyam.mishra@gmail.com>
2026-05-29 22:55:15 +0530

    Initial commit

---
+++ src/links.js
@@ -0,0 +1,5 @@
+export const outboundLinks = {
+  installer: "https://example.com/hushline/Hushline-Setup.exe",
+  repository: "https://github.com/example/hushline",
+  releases: "https://github.com/example/hushline/releases",
+};
